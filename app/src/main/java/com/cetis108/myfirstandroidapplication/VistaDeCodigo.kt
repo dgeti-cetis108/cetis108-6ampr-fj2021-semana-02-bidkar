a3dce66198292c551076f6a0b25f30a631c7bd34
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-02-26 08:42:23 -0700

    Vista de Diseño
    Atributos importantes:
    - alto: layout_height
    - ancho: layout_width
    - id: @+id/miId

---
+++ app/src/main/java/com/cetis108/myfirstandroidapplication/VistaDeCodigo.kt
@@ -0,0 +1,13 @@
+package com.cetis108.myfirstandroidapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VistaDeCodigo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_de_codigo)
+
+         // R.id.etVistaDeCodigoNombre
+    }
+}
